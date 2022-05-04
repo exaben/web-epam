cff25d6f65c2587532ead4d2e0eea73c5a58ae99
Gál Bence <74590411+exaben@users.noreply.github.com>
2022-05-04 17:31:00 +0200

    Delete datingapp directory

--- datingapp/src/main/java/hu/nye/project/datingapp/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package hu.nye.project.datingapp.repository;
-
-import hu.nye.project.datingapp.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
