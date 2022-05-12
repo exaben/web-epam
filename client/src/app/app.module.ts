f426bbf00a0b397ed73104d79706795036445508
agiszokolai <agi.szokolai@gmail.com>
2022-05-12 18:14:17 +0200

    components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,13 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { SearchComponent } from './components/search/search.component';
+import { NavbarComponent } from './reusables/navbar/navbar.component';
+import { PeopleProfileComponent } from './components/search/people-profile/people-profile.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistrationComponent,
+    ProfileComponent,
+    SearchComponent,
+    NavbarComponent,
+    PeopleProfileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ client/src/app/components/search/people-profile/people-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-people-profile',
+  templateUrl: './people-profile.component.html',
+  styleUrls: ['./people-profile.component.css']
+})
+export class PeopleProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
