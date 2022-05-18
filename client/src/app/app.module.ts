a19eaf309d58a958012dc5941dc625e423cdc510
katonatamas99 <78899992+katonatamas99@users.noreply.github.com>
2022-05-18 16:02:26 +0200

    regisztráció login

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http'
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -34,6 +34,8 @@ import { PeopleModalComponent } from './components/search/people-modal/people-mo
     AppRoutingModule,
     HttpClientModule,
     NgbModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/components/registration/registration.component.ts
+++ client/src/app/components/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
+  registerForm!: FormGroup;
 
   constructor() { }
 
-  ngOnInit(): void {
+  onSubmit(): void{
+    console.log(this.registerForm);
   }
+  ngOnInit(): void {
+    this.registerForm = new FormGroup({
+      firstname: new FormControl(null, [Validators.required]),
+      lastname: new FormControl(null, [Validators.required]),
+      username: new FormControl(null, [Validators.required]),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      birthdate: new FormControl(null, [Validators.required]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(5), Validators.pattern('^(?=.[a-z])(?=.[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$')]),
+      pswrepeat: new FormControl(null, [Validators.required, this.samePasswordValidators.bind(this)])
 
+    });
+  }
+  samePasswordValidators(control: FormControl): { [s: string]: boolean } | null {
+    return this.registerForm !== undefined && this.registerForm.get('password')!.value === control.value
+      ? null
+      : { notSamePassword: true }
+  }
 }
