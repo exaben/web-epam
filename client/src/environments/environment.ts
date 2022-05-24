550857ab082477e1c7454812492d2fc1a8e6d9d5
katonatamas99 <78899992+katonatamas99@users.noreply.github.com>
2022-05-24 12:12:21 +0200

    .

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -6,6 +6,7 @@ export const environment = {
   production: false,
 };
 
+// backend url-je itt van összekötve a frontend és a backend
 export const API_URL = 'http://localhost:8080';
 /*
  * For easier debugging in development mode, you can import the following file
