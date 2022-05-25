8e07fb65ba102d0f8a3d7372a2d0180d7061776c
exaben <gallaci5@gmail.com>
2022-05-25 10:18:07 +0200

    modisitas

--- client/src/app/services/matching.service.ts
+++ client/src/app/services/matching.service.ts
@@ -1,9 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { MatchedType } from 'src/app/models/matched';
+import { API_URL } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MatchingService {
+  url: string = API_URL;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.url += "/matched" ;
+   }
+
+  getAll(): Observable<MatchedType[]>{
+    return this.http.get(this.url) as Observable<MatchedType[]>;
+  }
+
+  getById(id: number): Observable<MatchedType[]>{
+    return this.http.get(`${this.url}/${id}`) as Observable<MatchedType[]>;
+  }
+
+  post(user: MatchedType): Observable<MatchedType> {
+    return this.http.post<MatchedType>(this.url,user);
+  }
+
+  delete(id: number) {
+    return this.http.delete(`${this.url}/${id}`)
+  }
 }
